ef74983897ef6e3af407f445161ed7f055c04259
RyujiKitaoka <ryuji.kitaoka0824@gmail.com>
2024-11-24 20:39:53 +0900

    Initial Commit

---
+++ Fleats.Github/Fleats_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fleats_GithubApp.swift
+//  Fleats.Github
+//
+//  Created by 北岡隆志 on 2024/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Fleats_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fleats.GithubTests/Fleats_GithubTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fleats_GithubTests.swift
+//  Fleats.GithubTests
+//
+//  Created by 北岡隆志 on 2024/11/24.
+//
+
+import Testing
+@testable import Fleats_Github
+
+struct Fleats_GithubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
